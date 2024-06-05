ce9681e09e21ecf60d46fc31c88603c05b629d56
mateustui <mateusfalcao99@hotmail.com>
2024-06-05 09:34:41 -0300

    gh-pages

--- src/models/index.js
+++
@@ -1,6 +0,0 @@
-export {BeeGLTF} from './BeeGLTF';
-export {HQ_2} from './HQ_2';
-export {HQ_3} from './HQ_3';
-export {Fox} from './Fox';
-export {HiveAImodel} from './HiveAImodel';
-export {MineBee} from './MineBee';
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/3D_landingpage",
-  plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-})
